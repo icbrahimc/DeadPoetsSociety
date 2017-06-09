acff850eddc183a0f4038f1ba47fae365679e939
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-06-09 02:04:24 -0700

    Added the books model to the code.

---
+++ MorehouseDPS/Books.swift
@@ -0,0 +1,25 @@
+//
+//  Books.swift
+//  MorehouseDPS
+//
+//  Created by icbrahimc on 6/8/17.
+//  Copyright © 2017 icbrahimc. All rights reserved.
+//
+
+import UIKit
+
+/*
+ This is the book class. This is where the data for books is populated and sent
+ to the database.
+*/
+class Books: NSObject {
+    var title: NSString?
+    var author: NSString?
+    var numberOfCopies: NSNumber?
+    
+    init(newTitle: NSString, newAuthor: NSString, newNumber: NSNumber) {
+        title = newTitle
+        author = newAuthor
+        numberOfCopies = newNumber
+    }
+}
