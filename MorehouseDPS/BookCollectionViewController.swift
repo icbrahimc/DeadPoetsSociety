f82a0c5106c7c5c2f9f9df96178d7b52b90686fc
icbrahimc <icbrahimc@Ibrahims-MacBook-Pro.local>
2017-08-19 02:31:06 -0400

    Delete stuff

--- MorehouseDPS/BookCollectionViewController.swift
+++ MorehouseDPS/BookCollectionViewController.swift
@@ -13,12 +13,9 @@ import UIKit
 
 private let reuseIdentifier = "bookCollectionViewCell"
 fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-fileprivate var searches = ["Hey", "Soul", "Sister", "Mama", "Ibrahim"]
 fileprivate let itemsPerRow: CGFloat = 3
 
 class BookCollectionViewController: DatasourceController {
-
-//    var books: [Books] = [Books(author: "Ibrahim", title: "Conteh", copies: 3, imageURL: "huh")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,30 +34,6 @@ class BookCollectionViewController: DatasourceController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    // MARK: UICollectionViewDataSource
-
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of items
-//        return books.count
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! BooksCell
-//        // Configure the cell
-//        cell.book = books[indexPath.row]
-//        return cell
-//    }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if let bookData = self.datasource?.item(indexPath) as? Books {
@@ -101,35 +74,4 @@ class BookCollectionViewController: DatasourceController {
                         minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInsets.left
     }
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 }
